2a44f5a2cefdd4b47fdb16bc9d2b850fe1a72339
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-06-18 13:48:19 +0530

    add user message page

---
+++ src/app/u/[username]/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+const MessagePage = () => {
+  return (
+    <div>
+      <Navbar />
+      Message
+    </div>
+  );
+};
+
+export default MessagePage;
