bf3d88d23535b536cd556210fcbd47dc2e764894
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-06-18 17:22:29 +0530

    refactor get-message route

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -1,72 +1,48 @@
-import { getServerSession, User } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/options";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/User";
 import mongoose from "mongoose";
+import { User } from "next-auth";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/options";
 
-export async function POST(request: Request) {
+export async function GET(request: Request) {
   await dbConnect();
-
   const session = await getServerSession(authOptions);
-  const user: User = session?.user as User;
+  const _user: User = session?.user as User;
 
-  if (!session || !session.user) {
+  if (!session || !_user) {
     return Response.json(
-      {
-        success: false,
-        message: "You must be logged in to request a new verification code",
-      },
-      {
-        status: 401,
-      }
+      { success: false, message: "Not authenticated" },
+      { status: 401 }
     );
   }
-
-  const userId = new mongoose.Types.ObjectId(user._id);
+  const userId = new mongoose.Types.ObjectId(_user._id);
   try {
     const user = await UserModel.aggregate([
       { $match: { _id: userId } },
       { $unwind: "$messages" },
       { $sort: { "messages.createdAt": -1 } },
-      {
-        $group: {
-          _id: "$_id",
-          messages: { $push: "messages" },
-        },
-      },
-    ]);
+      { $group: { _id: "$_id", messages: { $push: "$messages" } } },
+    ]).exec();
 
-    if (!user || user.length === 0) {
+    if (!user) {
       return Response.json(
-        {
-          success: false,
-          message: "User not found",
-        },
-        {
-          status: 404,
-        }
+        { message: "User not found", success: false },
+        { status: 404 }
       );
     }
 
     return Response.json(
-      {
-        success: true,
-        messages: user[0].messages,
-      },
+      { messages: user[0].messages },
       {
         status: 200,
       }
     );
-  } catch (error: any) {
-    console.error("Unexpected Error occured: ", error);
+  } catch (error) {
+    console.error("An unexpected error occurred:", error);
     return Response.json(
-      {
-        success: false,
-        message: error.message,
-      },
-      {
-        status: 500,
-      }
+      { message: "Internal server error", success: false },
+      { status: 500 }
     );
   }
 }
